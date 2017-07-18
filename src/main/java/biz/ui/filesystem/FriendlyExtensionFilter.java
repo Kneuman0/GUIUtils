d3b859a5723aa888f5d2d3618f5f54ba440f3423
Kneuman0 <kylepneuman@gmail.com>
2017-07-18 00:58:39 -0400

    Refactored imports

--- src/main/java/biz/ui/filesystem/FriendlyExtensionFilter.java
+++ src/main/java/biz/ui/filesystem/FriendlyExtensionFilter.java
@@ -2,7 +2,6 @@ package biz.ui.filesystem;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
